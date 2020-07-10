8b4e0645d51a8516d28b103f16c23ca188fc7b30
MrC74rk <bryan.angelo@gmail.com>
2020-07-10 14:17:12 -0500

    Added Navigation link to the post view so demo can be viewed directly from a device or simulator

--- JoesDemo/ContentView.swift
+++ JoesDemo/ContentView.swift
@@ -12,14 +12,38 @@ import SwiftUI
 
 struct ContentView: View {
  
-
+@State private var isActive = false
     var body: some View {
-            
-        Text("Hello World")
-
-    
+        NavigationView {
+            Section {
+                VStack {
+                    Text("You're looking at the wrong view.")
+                    Text("Preview the file:")
+                    Text("PostList.swift")
+                        .font(.title)
+                        .bold()
+                    NavigationLink(destination: PostList(), isActive: self.$isActive){
+                        Text("")
+                    }
+                        
+                    Button("Go to view") {
+                        self.isActive = true
+                    }
+                    .font(.title)
+                    .foregroundColor(.blue)
+                    .background(Color.white)
+                    .cornerRadius(40)
+                    .padding(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius:40)
+                        .stroke(Color.blue, lineWidth: 5))
+                    
+                }
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- JoesDemo/PostList.swift
+++ JoesDemo/PostList.swift
@@ -11,10 +11,12 @@ import SwiftUI
 struct PostList: View {
     
     @State var posts: [Post] = []
+    
     var body: some View {
         List(posts) { post in
             Text(post.title)
         }
+        .navigationBarTitle("API Output")
         .onAppear {
             Api().getPosts { (posts) in
             self.posts = posts
@@ -25,6 +27,8 @@ struct PostList: View {
 
 struct PostList_Previews: PreviewProvider {
     static var previews: some View {
+        NavigationView {
         PostList()
+        }
     }
 }
